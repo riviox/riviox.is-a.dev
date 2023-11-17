e5bf2f539e56c5a47cfce07776a99c88bf05a4ba
Riviox <100956266+RivioxGaming@users.noreply.github.com>
2023-11-17 16:08:21 +0100

    Update index.html

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const cnt = document.getElementById("container");
+cnt.addEventListener('dblclick', gg);
+
+function gg() {
+  const cnt = document.getElementById("container");
+  cnt.style.color = "black";
+  cnt.style.backgroundImage = "url(https://pbs.twimg.com/media/FS5XAY3X0AE6VLh.png)";
+}
+
